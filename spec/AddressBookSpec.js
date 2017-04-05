15dd523be221993860017e3541e59edb4f0bb779
TRI VI <trivi2710@gmail.com>
2017-04-05 12:01:12 -0400

    adding an asynchronous test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -19,4 +19,13 @@ describe('Address Book', function() {
 
 		expect(addressBook.getContact(0)).not.toBeDefined();
 	});	
-})
+});
+
+describe('Async Address Book', function() {
+	if('should grab initial contacts', function() {
+		var addressBook = new AddressBook();
+
+		addressBook.getInitialContacts();
+		expect(addressBook.initialComplete).toBe(true);
+	});
+});
